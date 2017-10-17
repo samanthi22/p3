32189d09391b00255e278d99c28dba6d3c08b8d5
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2017-10-17 14:49:52 -0700

    Moved errors to child view

--- resources/views/bill/amount.blade.php
+++ resources/views/bill/amount.blade.php
@@ -8,6 +8,20 @@
  	<link rel="stylesheet" type="text/css" href="/css/bill.css">
 @endpush
 
+@section('errors')
+
+	<?php if(!empty($errors)) : ?>
+		<div class="error">
+			<ul>
+				<?php foreach($errors as $error) : ?>
+					<li><?=$error?></li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+	<?php endif ?>
+
+@endsection
+
 @section('content')
 		<div class='display'>
 		Everyone owes <span class='amount'>$$</span>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,30 +12,24 @@
 <body>
 <h1>Bill Splitter</h1>
 
+<form method='GET' action='/'>
+	<p><label for='people'>Split how many ways?</label><input type="text" name="people" id="people" value=''></p>
 
-<p><label for='people'>Split how many ways?</label><input type="text" name="people" id="people" value=''></p>
+	<p><label for='tab'>How much was the tab? </label><input type="text" name="tab" id="tab" value=''></p>
 
-<p><label for='tab'>How much was the tab? </label><input type="text" name="tab" id="tab" value=''></p>
+	<p><label for='tip_amount'>How was the service?</label><select name='tip_amount' id='tip_amount'><option value="18">Good (18% tip) </option><option value="20"> Excellent (20% tip) </option>
+	</select></p>
 
-<p><label for='tip_amount'>How was the service?</label><select name='tip_amount' id='tip_amount'><option value="18">Good (18% tip) </option><option value="20"> Excellent (20% tip) </option>
-</select></p>
+	<p><b>Round up?</b> <input type="checkbox" name="round_up" id="round_up" value="yes"><label for="round_up"> Yes </label></p>
 
-<p><b>Round up?</b> <input type="checkbox" name="round_up" id="round_up" value="yes"><label for="round_up"> Yes </label></p>
+	<p>
+	<input type="submit" value="Calculate"></p>
 
-<p>
-<input type="submit" value="Calculate"></p>
+	</form>
 
-</form>
-
-<?php if(!empty($errors)) : ?>
-	<div class="error">
-		<ul>
-			<?php foreach($errors as $error) : ?>
-				<li><?=$error?></li>
-			<?php endforeach; ?>
-		</ul>
-	</div>
-<?php endif ?>
+<section>
+	@yield('errors')
+</section>
 
 <section>
 	@yield('content')
