5f36526af77bc422dce69dc71981ff107943d2c4
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2017-10-22 13:41:30 -0700

    Changed routes file

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,4 @@
 |
 */
 
-// Route::get('/env', function () {
-//     dump(config('app.name'));
-//     dump(config('app.env'));
-//     dump(config('app.debug'));
-//     dump(config('app.url'));
-// });
-
 Route::get('/form', 'WelcomeController@form');
-Route::get('/amount', 'WelcomeController@amount');
-
-// Route::get('/debugbar', function () {
-
-//     $data = ['foo' => 'bar'];
-//     Debugbar::info($data);
-//     Debugbar::info('Current environment: '.App::environment());
-//     Debugbar::error('Error!');
-//     Debugbar::warning('Watch out…');
-//     Debugbar::addMessage('Another message', 'mylabel');
-
-//     return 'Just demoing some of the features of Debugbar';
-// });
